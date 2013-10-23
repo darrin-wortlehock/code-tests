76ea55daed2d3e342ccaddd3e4293579ffdf7066
Darrin Wortlehock <darrin@exempla.co.uk>
2013-10-23 15:05:52 +0100

    word helper passes spec

--- quipper/lib/word_helper.rb
+++ quipper/lib/word_helper.rb
@@ -5,12 +5,22 @@ module WordHelper
   extend self
 
   def tally(phrase)
+    phrase.split.inject(Hash.new(0)) do |hash, word|
+      hash[word.to_sym] += 1
+      hash
+    end
   end
 
   def filter(phrase, blacklist)
+    blacklist.split.inject(phrase) do |memo, word_to_redact|
+      redact_pattern = /\b#{word_to_redact}\b/i
+      memo.gsub( redact_pattern, '*' * word_to_redact.length )
+    end
   end
 
   def link_to_users(message, domain)
+    user_pattern = /@(\w+)/
+    message.gsub(user_pattern, "<a href='#{domain}\\1'>@\\1</a>")
   end
 
 end
